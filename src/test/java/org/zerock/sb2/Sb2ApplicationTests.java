5e3862ea8df7570113f52f685692174f9f471eaa
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-03-31 11:39:01 +0900

    🎉 Initial commit of Spring Boot project

---
+++ src/test/java/org/zerock/sb2/Sb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.sb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
