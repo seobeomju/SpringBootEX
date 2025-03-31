20f70a120f7910c9f2eba20a0180d04df3b8b67a
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-03-31 14:17:10 +0900

    CRUDTest 추가

--- src/test/java/org/zerock/sb2/todo/TodoRepoTests.java
+++ src/test/java/org/zerock/sb2/todo/TodoRepoTests.java
@@ -1,20 +1,25 @@
 package org.zerock.sb2.todo;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.zerock.sb2.todo.entities.Todo;
 import org.zerock.sb2.todo.repository.TodoRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
 
 @SpringBootTest
 @Log4j2
+@Transactional
 public class TodoRepoTests {
     
     @Autowired(required = false)
     private TodoRepository repository;
 
+    // @Disabled
     @Test
     public void testInsert(){
         //insert는 save()하시면 끝
@@ -25,5 +30,26 @@ public class TodoRepoTests {
         .build();
 
         repository.save(todo);
+    }
+    @Test
+    public void testRead(){
+        java.util.Optional<Todo> result = repository.findById(1L);
+
+        log.info(result.get());
+    }
+
+    @Test
+    public void testDelete(){
+        Long tno = 1L;
+        repository.deleteById(tno);
+    }
+
+    @Test
+    @Commit
+    public void testUpdate(){
+        java.util.Optional<Todo> result = repository.findById(2L);
+        Todo todo = result.get();
+        todo.changeTitle("change Title 234");
+
     }
 }
