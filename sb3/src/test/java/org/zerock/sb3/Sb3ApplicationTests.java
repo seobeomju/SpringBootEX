580855af9a1643a5132196e24422329068924197
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-01 23:29:49 +0900

    sb3 생성성

---
+++ sb3/src/test/java/org/zerock/sb3/Sb3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.sb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
