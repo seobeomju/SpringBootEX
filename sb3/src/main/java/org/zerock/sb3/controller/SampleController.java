d8132b2b811a917eef9b15878ad4ca12b276b382
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-01 23:45:31 +0900

    Setting

---
+++ sb3/src/main/java/org/zerock/sb3/controller/SampleController.java
@@ -0,0 +1,14 @@
+package org.zerock.sb3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample")
+public class SampleController {
+    @GetMapping("ex1")
+    public void ex1(){
+        
+    }
+}
