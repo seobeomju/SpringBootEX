9cef6b733086578a9c63e4727bd64d6f860716d1
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-01 23:55:27 +0900

    반복 두가지 방법

--- sb3/src/main/java/org/zerock/sb3/controller/SampleController.java
+++ sb3/src/main/java/org/zerock/sb3/controller/SampleController.java
@@ -1,6 +1,7 @@
 package org.zerock.sb3.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample")
 public class SampleController {
     @GetMapping("ex1")
-    public void ex1(){
-        
+    public void ex1(Model model){
+
+        String[] arr = new String[]{"AAA","BBBB","CCC"};
+        model.addAttribute("list", arr);
+
     }
 }
