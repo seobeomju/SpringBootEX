1ddbccd3d1fa8dad0a831fe16991f2e4c3040420
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-02 13:23:40 +0900

    reply table 생성

--- sb2/src/main/java/org/zerock/sb2/board/dto/BoardReadDTO.java
+++ sb2/src/main/java/org/zerock/sb2/board/dto/BoardReadDTO.java
@@ -9,13 +9,13 @@ public class BoardReadDTO {
     private Long bno;
     private String title,content, writer;
     private boolean delFlag;
-    private int viewCnt;
+    private long viewCnt;
     private LocalDateTime regDate, modDate;
    
    
    //bno,title, content,  writer,  delFlag,  viewCnt,  regDate, modDate
    
-    public BoardReadDTO(Long bno, String title, String content, String writer, boolean delFlag, int viewCnt,
+    public BoardReadDTO(Long bno, String title, String content, String writer, boolean delFlag, long viewCnt,
             LocalDateTime regDate, LocalDateTime modDate) {
         this.bno = bno;
         this.title = title;
---
+++ sb2/src/main/java/org/zerock/sb2/reply/entities/ReplyEntity.java
@@ -0,0 +1,55 @@
+package org.zerock.sb2.reply.entities;
+ 
+ import java.time.LocalDateTime;
+ 
+ import org.springframework.data.annotation.CreatedDate;
+ import org.springframework.data.annotation.LastModifiedDate;
+ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.zerock.sb2.board.entities.BoardEntity;
+
+import jakarta.persistence.Column;
+ import jakarta.persistence.Entity;
+ import jakarta.persistence.EntityListeners;
+ import jakarta.persistence.GeneratedValue;
+ import jakarta.persistence.GenerationType;
+ import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+ import lombok.AllArgsConstructor;
+ import lombok.Builder;
+ import lombok.Getter;
+ import lombok.NoArgsConstructor;
+ import lombok.ToString;
+ 
+ @EntityListeners(value = AuditingEntityListener.class)
+ @Entity
+ @Table(name = "tbl_reply", indexes ={
+  
+ })
+ @Getter
+ @ToString
+ @Builder
+ @AllArgsConstructor
+ @NoArgsConstructor
+ public class ReplyEntity {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long rno;
+
+
+   private String replyText;
+
+ 
+   private String replyer;
+   @ManyToOne
+   private BoardEntity board;
+
+   @CreatedDate
+   @Column(name="regDate", updatable = false)
+   protected LocalDateTime regDate;
+
+   @LastModifiedDate
+   @Column(name ="moddate" )
+   protected LocalDateTime modDate;
+
+ }
