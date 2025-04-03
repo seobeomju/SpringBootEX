bfd9b0f52c19bb00fddbb3308d3aa857c6eddb97
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-03 13:31:05 +0900

    Reply service addDTO

--- sb2/src/main/java/org/zerock/sb2/reply/dto/ReplyAddDTO.java
+++ sb2/src/main/java/org/zerock/sb2/reply/dto/ReplyAddDTO.java
@@ -1,6 +1,20 @@
 package org.zerock.sb2.reply.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class ReplyAddDTO {
 
 
+    @NotBlank
+    private String replyText;
+
+    @NotBlank
+    private String replyer;
+
+    @NotBlank
+    private Long bno;
 }
--- sb2/src/main/java/org/zerock/sb2/reply/service/ReplyService.java
+++ sb2/src/main/java/org/zerock/sb2/reply/service/ReplyService.java
@@ -1,6 +1,18 @@
 package org.zerock.sb2.reply.service;
 
-public interface ReplyService {
+import org.zerock.sb2.board.entities.BoardEntity;
+import org.zerock.sb2.reply.dto.ReplyAddDTO;
+import org.zerock.sb2.reply.entities.ReplyEntity;
 
+public interface ReplyService {
+    Long add(ReplyAddDTO addDTO);
+
+    default ReplyEntity dtoToEntity(ReplyAddDTO addDTO) {
+        return ReplyEntity.builder()
+                .replyText(addDTO.getReplyText())
+                .replyer(addDTO.getReplyer())
+                .board(BoardEntity.builder().bno(addDTO.getBno()).build())
+                .build();
+    }
 
 }
