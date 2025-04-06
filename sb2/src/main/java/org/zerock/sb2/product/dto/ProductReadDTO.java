eb13e1ea7c97498f0712c9e90cd42fbd4842bfcf
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-06 23:42:43 +0900

    ProductReadDTO 생성

---
+++ sb2/src/main/java/org/zerock/sb2/product/dto/ProductReadDTO.java
@@ -0,0 +1,27 @@
+package org.zerock.sb2.product.dto;
+
+import lombok.Data;
+import org.zerock.sb2.product.entities.ProductEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class ProductReadDTO {
+    private Long pno;
+
+    private String pname;
+
+    private int price;
+
+    private List<String> imageNames;
+
+    public ProductReadDTO(ProductEntity entity){
+        this.pno = entity.getPno();
+        this.pname = entity.getPname();
+        this.price = entity.getPrice();
+        this.imageNames = entity.getImages().stream()
+                .map(pi -> pi.getImgname()).collect(Collectors.toList());
+    }
+}
