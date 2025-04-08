f2e6b27321c5ca21c4fe8be6bcbf9b95f0f271c2
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-08 11:10:04 +0900

    BoardRepo Query

--- sb7/src/main/java/org/zerock/sb7/board/repo/BoardRepo.java
+++ sb7/src/main/java/org/zerock/sb7/board/repo/BoardRepo.java
@@ -1,7 +1,21 @@
 package org.zerock.sb7.board.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.zerock.sb7.board.domain.Board;
 
 public interface BoardRepo extends JpaRepository<Board, Integer> {
+
+    //Pageable이 파라미터 무조건 리턴타입은 Page타입
+
+    @Query("""
+    select b.bno, b.title, b.writer, count(r), count(f)
+    from Board b 
+        left join Reply r on r.board = b
+        left join Favorite f on f.board = b
+    group by b.bno
+""")
+    Page<Object[]> list1(Pageable pageable);
 }
--- sb7/src/main/java/org/zerock/sb7/board/repo/FavoriteRepo.java
+++ sb7/src/main/java/org/zerock/sb7/board/repo/FavoriteRepo.java
@@ -1,7 +1,9 @@
 package org.zerock.sb7.board.repo;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.sb7.board.domain.Favorite;
 
 public interface FavoriteRepo extends JpaRepository<Favorite, Integer> {
+
 }
