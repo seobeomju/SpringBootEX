5454479005d45e6695220f74d9152755bf25b32a
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-08 09:57:25 +0900

    Add templates folder and initial files

---
+++ sb7/src/main/java/org/zerock/sb7/Sb7Application.java
@@ -0,0 +1,13 @@
+package org.zerock.sb7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb7Application.class, args);
+	}
+
+}
---
+++ sb7/src/test/java/org/zerock/sb7/Sb7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.sb7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
