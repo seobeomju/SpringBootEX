fab8c066e3f6ca0ac0e53e0db1b0f43860495ee2
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-10 14:24:07 +0900

    file업로드 설정4

--- sb7/src/main/java/org/zerock/sb7/util/FileUploadUtil.java
+++ sb7/src/main/java/org/zerock/sb7/util/FileUploadUtil.java
@@ -1,21 +1,29 @@
 package org.zerock.sb7.util;
 
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Log4j2
-public class FileUploadUtil {
+@RequiredArgsConstructor
+public class FileUploadUtil  {
 
     @Value("${org.zerock.upload}")
     private String uploadDir;
 
     @PostConstruct
-    public void ready(){
+    public void ready()throws IOException {
         log.info("-------post construct-------------");
         log.info("uploadDir:" + uploadDir);
 
@@ -23,7 +31,12 @@ public class FileUploadUtil {
         if(!uploadDirFile.exists()){
             uploadDirFile.mkdirs();
         }
-
-
     }
+
+    public List<String> uploadFiles(List<MultipartFile> files) throws IOException {
+
+        List<String> uploadedFiles = new ArrayList<>();
+        return uploadedFiles;
+    }
+
 }
