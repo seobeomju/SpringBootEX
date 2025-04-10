d51d7b681e596da3a816162b6c1eef182cd41e8e
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-10 13:35:03 +0900

    login error, logout

--- sb7/src/main/java/org/zerock/sb7/member/controller/MemberController.java
+++ sb7/src/main/java/org/zerock/sb7/member/controller/MemberController.java
@@ -3,8 +3,10 @@ package org.zerock.sb7.member.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/member")
@@ -12,7 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MemberController {
     @GetMapping("login")
-    public void login(){
+    public void login(@RequestParam(required = false, name = "error") String error,
+                      @RequestParam(required = false, name = "logout") String logout,
+                      Model model) {
         log.info("GET login");
+        log.info("error: " + error);
+        log.info(error == null ? "null" : "exist");
+
+        if(error != null) {
+            model.addAttribute("msg", "Login failed");
+        }
+        if(logout != null) {
+            model.addAttribute("logout", "Logged out!");
+        }
     }
 }
